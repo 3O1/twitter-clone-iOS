57a2f8a9afe1e1d280bb7c07054ff1f43cbbc15b
3O1 <jamesbrandonedwards301@gmail.com>
2021-07-23 19:25:39 -0400

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by James on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
